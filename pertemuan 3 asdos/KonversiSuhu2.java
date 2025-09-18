df394ad591a6af1d986d1eea882dfd189d867d8f
tayadiHAhmad <ahmad010hidayat@gmail.com>
2025-09-18 17:49:38 +0700

    Update KonversiSuhu2.java

--- pertemuan 3 asdos/KonversiSuhu2.java
+++ pertemuan 3 asdos/KonversiSuhu2.java
@@ -1,6 +1,7 @@
-public class KonversiSuhu2 extends KonversiSuhu {
+public class KonversiSuhu1 extends KonversiSuhu {
     public double fahrenheitToReamur(double f) {
         double c = (f - 32) * 5 / 9; // Fahrenheit ke Celcius
         return c * 4 / 5;            // Celcius ke Reamur
     }
+
 }
